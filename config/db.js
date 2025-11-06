096514f4b2d47ca4f884692723717acaf48d5654
F. Franklin Joshua <franklinjoshua2735@gmail.com>
2025-11-06 18:52:17 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,6 +2,10 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
+    if (!process.env.MONGO_URL) {
+      throw new Error("MONGO_URL is not defined in environment variables.");
+    }
+    console.log(process.env.MONGO_URL)
     await mongoose.connect(process.env.MONGO_URL);
     console.log("MongoDB Connected");
   } catch (err) {
